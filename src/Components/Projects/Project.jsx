51feb3dd881e6446cb7638444ce4d0114b9f9951
Ramiro <ramifazio@gmail.com>
2022-06-15 15:53:47 -0300

    Add carousel to projects

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-//import { StyledContainer } from "../../styles/globalStyles.js";
+import { StyledContainer, StyledLink } from "../../styles/globalStyles.js";
 
 function Project({ name, description, link }) {
   return (
-    <div>
-      <a href={link} target="_blank" rel="noreferrer">
+    <StyledContainer>
+      <StyledLink href={link} target="_blank" rel="noreferrer">
         <h1>{name}</h1>
         <p>{description}</p>
-      </a>
-    </div>
+      </StyledLink>
+    </StyledContainer>
   );
 }
 
 export default Project;
+
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { projectsData } from "./projectsData";
 import Project from "./Project";
+import Carousel from "react-elastic-carousel";
+import { StyledCarouselContainer } from "../../styles/globalStyles";
 
 export default function Projects() {
   return (
-    <div>
-      {projectsData.map((p, index) => {
-        return (
-          <div key={index}>
-            <Project name={p.name} description={p.description} link={p.link} />
-          </div>
-        );
-      })}
-    </div>
+    <StyledCarouselContainer>
+      <Carousel
+        pagination={false}
+        showArrows={true}
+        enableAutoPlay={true}
+        autoPlaySpeed={4000}
+      >
+        {projectsData.map((p, index) => {
+          return (
+            <div key={index}>
+              <Project
+                name={p.name}
+                description={p.description}
+                link={p.link}
+              />
+            </div>
+          );
+        })}
+      </Carousel>
+    </StyledCarouselContainer>
   );
 }
