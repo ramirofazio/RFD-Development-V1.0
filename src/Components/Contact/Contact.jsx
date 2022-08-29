3de2779a5c9ba406feecbe609a75d8545176a506
Ramiro Fazio Dattoli <70992145+ramirofazio@users.noreply.github.com>
2022-08-29 14:07:20 -0300

    Contact Text Details

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -74,7 +74,7 @@ function Contact() {
         <StyledP>
           I&apos;m interested in freelance opportunities, especially ambitious
           or large projects. However, if you have other request or question,
-          don&apos;t hesitate to send mi an E-mail!
+          don&apos;t hesitate to send an E-mail!
         </StyledP>
         <a href="mailto: ramifazio@gmail.com" style={{ width: "30%" }}>
           <StyledButton type="submit" value="Send" />
